2439d1f1fe9bf80fa0f59d284248bf1b6bc6ef70
spiddacino <chidi.okonta@gmail.com>
2017-03-21 22:18:11 +0100

    Commit

--- NahcoTrack/models.py
+++ NahcoTrack/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.db.models import Count
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,12 +17,37 @@ class Location(models.Model): # List of Sheds/Warehouse pay points
     )
     location = models.CharField(max_length=3, choices=LOCATION, default='IMP')
 
+    # Metadata
+    class Meta:
+        ordering = ["-location"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Location.
+        """
+        return reverse('location-detail', args=[str(self.id)])
+
+    # def __str__(self):
+    #     return self.location
+
     def __unicode__(self):
         return self.location
 
 
 class Software(models.Model): # List the software in use
     software = models.CharField(max_length=6)
+
+    # Metadata
+    class Meta:
+        ordering = ["-software"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('software-detail', args=[str(self.id)])
 
     def __unicode__(self):
         return self.software
@@ -38,7 +64,18 @@ class Bank(models.Model):
         ('Fidelity', 'Fidelity Bank'),
         ('Stanbic', 'Stanbic IBTC')
     )
-    banks = models.CharField(max_length=10, choices=BANKS, default='Access')
+    banks = models.CharField(max_length=10, choices=BANKS, default='Skye')
+
+    # Metadata
+    class Meta:
+        ordering = ["-banks"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Location.
+        """
+        return reverse('bank-detail', args=[str(self.id)])
 
     def __unicode__(self):
         return self.banks
@@ -51,43 +88,60 @@ class AgencyDetail(models.Model):
     )
     agency_name = models.CharField(max_length=50)
     agency_type = models.CharField(max_length=2, choices=AGENCY_TYPE)
-    agency_minimum_balance = models.DecimalField(decimal_places=2,max_digits=10)
-    agency_current_balance = models.DecimalField(decimal_places=2,max_digits=10)
+    agency_minimum_balance = models.DecimalField(decimal_places=2,max_digits=10, null=True)
+    agency_current_balance = models.DecimalField(decimal_places=2,max_digits=10, null=True)
+
+    # Metadata
+    class Meta:
+        ordering = ["-agency_name"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('AgencyDetail-detail', args=[str(self.id)])
 
     def __unicode__(self):
         return self.agency_name
 
 
-class Staff(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    staff_no = models.CharField(max_length=4)
-    # set designation for access control
-
-    def __unicode__(self):
-        return self.staff_no
+# class Staff(models.Model):
+#     first_name = models.CharField(max_length=20)
+#     last_name = models.CharField(max_length=20)
+#     staff_no = models.CharField(max_length=4)
+#     # set designation for access control
+#
+#     def __unicode__(self):
+#         return self.staff_no
 
 
 class AgentAWBList(models.Model):
-    agency = models.ForeignKey(AgencyDetail)
+    agency = models.ForeignKey(AgencyDetail, on_delete=models.SET_NULL, null=True)
     airwaybill = models.CharField(max_length=11)
     awb_pieces = models.IntegerField()
     rcvd_pieces = models.IntegerField()
     awb_weight = models.FloatField(default=0.00)
     rcvd_weight = models.FloatField(default=0.00)
-    flight_date = models.DateTimeField()
-    input_date = models.DateTimeField()
-    exit_date = models.DateTimeField()
-    authorized_by = models.ForeignKey(Staff)
+    flight_date = models.DateField()
+    input_date = models.DateField()
+    exit_date = models.DateField()
+    awb_cleared = models.BooleanField(default=False)
+    #authorized_by = models.ForeignKey(Staff)
+
+    # Metadata
+    class Meta:
+        ordering = ["-exit_date"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('AgentAWBList-detail', args=[str(self.id)])
 
     def __unicode__(self):
         return self.airwaybill
-
-
-class PaymentUtilizations(models.Model):
-    awb_on_list = models.ForeignKey(AgentAWBList)
-    invoice_amount = models.DecimalField(decimal_places=2,max_digits=10)
-    authorized_by = models.ForeignKey(Staff)
 
 
 class Payments(models.Model):
@@ -97,44 +151,120 @@ class Payments(models.Model):
         ('DR', 'Bank Draft'),
         ('TR', 'Transfer')
     )
-    agency = models.ForeignKey(AgencyDetail)
-    payment = models.DecimalField(decimal_places=2,max_digits=10)
-    payment_date = models.DateTimeField()
-    payment_mode = models.CharField(max_length=2, choices=PAYMENTMODE)
-    teller_no = models.CharField(max_length=10)
-    authorized_by = models.ForeignKey(Staff)
+    bank = models.ForeignKey(Bank, on_delete=models.SET_NULL, null=True)
+    agency = models.ForeignKey(AgencyDetail, on_delete=models.SET_NULL, null=True)
+    payment_amount = models.DecimalField(decimal_places=2,max_digits=10)
+    payment_date = models.DateField()
+    payment_mode = models.CharField(max_length=2, choices=PAYMENTMODE, default='Cash')
+    teller_no = models.CharField(max_length=11)
+    #authorized_by = models.ForeignKey(Staff)
+
+    def __unicode__(self):
+        return self.teller_no
+
+    # Metadata
+    class Meta:
+        ordering = ["bank", "payment_date", "-teller_no"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('Payments-detail', args=[str(self.id)])
+
+    def __get_full_Lodgementdetail(self):
+        ":returns teller no, Amount, bank and payment/lodgement date"
+        return '%s (%s) %s, %s' % (self.teller_no,self.payment,self.bank,self.payment_date)
+    lodgement_detail = property(__get_full_Lodgementdetail)
+
+
+class PaymentUtilizations(models.Model):
+    payment = models.ForeignKey(Payments, on_delete=models.SET_NULL, null=True)
+    awb_on_list = models.ForeignKey(AgentAWBList, on_delete=models.SET_NULL, null=True)
+    invoice_amount = models.DecimalField(decimal_places=2, max_digits=10)
+    utilization_date = models.DateField(null=True)
+    #authorized_by = models.ForeignKey(Staff)
+
+    # Metadata
+    class Meta:
+        ordering = ["payment", "-utilization_date"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('PaymentUtilizations-detail', args=[str(self.id)])
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.utilization_date, self.payment__payments__teller_no)
 
 
 class TellerData(models.Model):
+    PAYMENTMODE = (
+        ('PO', 'POS'),
+        ('BA', 'Bank'),
+        ('TR', 'Transfer')
+    )
     location = models.ForeignKey(Location)
-    software = models.ForeignKey(Software, default='Galaxy')
-    teller_date = models.DateTimeField()
-    bank = models.ForeignKey(Bank)
-    agency = models.ForeignKey(AgencyDetail)  # used specially for MOU/credit agents
+    software = models.ForeignKey(Software, on_delete=models.SET_NULL, null=True, default='Galaxy')
+    teller_date = models.DateField()
+    bank = models.ForeignKey(Bank, on_delete=models.SET_NULL, null=True)
+    agency = models.ForeignKey(AgencyDetail, on_delete=models.SET_NULL, null=True)  # used specially for MOU/credit agents
     teller_no = models.CharField(max_length=10)
     teller_amount = models.DecimalField(decimal_places=2,max_digits=10)
-    authorized_by = models.ForeignKey(Staff)
+    payment_mode = models.CharField(max_length=2, choices=PAYMENTMODE, default='BA')
+    #authorized_by = models.ForeignKey(Staff)
+
+    # Metadata
+    class Meta:
+        ordering = ["software", "-bank", "teller_no", "teller_amount"]
+
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('TellerData-detail', args=[str(self.id)])
+
+    def __unicode__(self):
+        return '(%s) %s %s %s %s' % (self.software, self.teller_date, self.bank, self.teller_amount, self.teller_no)
 
 
-class DailySales(models.Model):
+class DailySales(models.Model): # Summary of a point daily sale
     SALESTYPE = (
         ('CR', 'Credit'),
         ('CA', 'Cash')
     )
     Location = models.ForeignKey(Location)
-    sales_date = models.DateTimeField()
+    sales_date = models.DateField()
     sales_type = models.CharField(max_length=2,choices=SALESTYPE)
     net_sales = models.DecimalField(decimal_places=2,max_digits=10)
     vat = models.DecimalField(decimal_places=2,max_digits=10)
     total_stamp = models.DecimalField(decimal_places=2,max_digits=10)
 
-class EventActions(models.Model):
-    event_code = models.CharField(max_length=3)
-    event_actions = models.CharField(max_length=30)
+    # Metadata
+    class Meta:
+        ordering = ["-sales_date", "Location"]
 
+    # Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Model.
+        """
+        return reverse('DailySales-detail', args=[str(self.id)])
 
-class EventTrail(models.Model): # Create an events audit trail
-    event_action = models.ForeignKey(EventActions)
-    event_action_success = models.BooleanField(default=1)
-    event_time = models.DateTimeField()
-    event_by = models.ForeignKey(Staff)
+    def __unicode__(self):
+        return '(%s) %s %s %s %s' % (self.sales_date, self.sales_type, self.net_sales, self.vat, self.total_stamp)
+
+# class EventActions(models.Model):
+#     event_code = models.CharField(max_length=3)
+#     event_actions = models.CharField(max_length=30)
+#
+#
+# class EventTrail(models.Model): # Create an events audit trail
+#     event_action = models.ForeignKey(EventActions)
+#     event_action_success = models.BooleanField(default=1)
+#     event_time = models.DateTimeField()
+#     #event_by = models.ForeignKey(Staff)
